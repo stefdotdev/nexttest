39d891a3b8c14f0594c676ce6cbbdb6ab9059b19
stbmbp <sbmbp@stbmbps-MacBook-Pro.local>
2024-08-03 20:52:15 +0900

    Added dashboard page chap 7

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,44 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { Card } from "@/app/ui/dashboard/cards"; // Import card component - defines what a Card looks like
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+import {
+  fetchRevenue,
+  fetchLatestInvoices,
+  fetchCardData, // Import the card Data
+} from "@/app/lib/data";
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  // Destructuring:
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData(); 
+  // receive Card Data into each of these Const variables
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard 
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* Send the Title, Value and Type data into the CARD component */}
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 }
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -3,47 +3,48 @@ import {
   ClockIcon,
   UserGroupIcon,
   InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+} from "@heroicons/react/24/outline"; // Import Icon components
+import { lusitana } from "@/app/ui/fonts";
 
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
   pending: ClockIcon,
   invoices: InboxIcon,
-};
+}; // Define key-value pair of Icon Names, readable in the context of a Card.
 
 export default async function CardWrapper() {
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
 
 export function Card({
+  // Define the type of data that should be received from the dashboard page
   title,
   value,
   type,
 }: {
   title: string;
   value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  type: "invoices" | "customers" | "pending" | "collected";
 }) {
-  const Icon = iconMap[type];
-
+  const Icon = iconMap[type]; // Match the Type data to the key-value pair of IconMap.
+  
   return (
+    // Having taken the data sent in by the Dashbaord page, display them here {title} etc.
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
       <div className="flex p-4">
+        {/* If there is an Icon, then display its name (therefor the icon component) */}
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
